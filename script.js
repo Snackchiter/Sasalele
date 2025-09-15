ceefb10aeef2ad41ed1538b627ddbd1775038efe
unknown <maks.pinaev.85@mail.ru>
2025-09-15 11:31:04 +0300

    sass

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 const jamk = document.getElementById('opr')
-const strokviv = document.getElementById('vivod')
 const AAAAH = new Audio('aah.mp3')
 
-function slozhen() {
-    strokviv.classList.toggle('bukva')
-    strokviv.textContent = "СКИБИДИ ДОП ДОП"
-    AAAAH.play()
-}
-jamk.addEventListener('click', slozhen)
+jamk.addEventListener('click', AAAAH.play())
