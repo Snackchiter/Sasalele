177b82383d1767802122a30d451b80dd57d45d8a
unknown <maks.pinaev.85@mail.ru>
2025-09-16 20:59:30 +0300

    s

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const jamk = document.getElementById('opr')
 const AAAAH = new Audio('aah.mp3')
-
+const scrimer = document.getElementById('scrimer')
 function slozhen() {
     AAAAH.play()
+    scrimer.classList.toggle('invisible')
 }
 
 jamk.addEventListener('click', slozhen)
