d445ee8624e62d88075d6384de2e5758d19a77a4
unknown <maks.pinaev.85@mail.ru>
2025-09-19 18:56:41 +0300

    s

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const jamk = document.getElementById('opr')
 const AAAAH = new Audio('aah.mp3')
 const scrimer = document.getElementById('scrimer')
+const fonch = document.getElementById('body')
 function slozhen() {
     AAAAH.loop = true
     AAAAH.play()
-    scrimer.classList.toggle('invisible')
+    setInterval(() => {
+        fonch.classList.add('fon')
+        setTimeout(() => {
+            fonch.classList.add('fonik')
+            setTimeout(() => {
+                fonch.classList.remove('fonik')
+            }, 50);
+        }, 50);
+    }, 100);
 }
 
 jamk.addEventListener('click', slozhen)
